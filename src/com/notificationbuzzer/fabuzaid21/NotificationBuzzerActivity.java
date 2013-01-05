8465b2efc7c3e4731ec5c96ba6bcb7ae36af58b4
Firas Abuzaid <fabuzaid21@gmail.com>
2013-01-05 10:58:36 -0800

    fixed layout bug so the headers work. formatted files for consistent style.

--- src/com/notificationbuzzer/fabuzaid21/NotificationBuzzerActivity.java
+++ src/com/notificationbuzzer/fabuzaid21/NotificationBuzzerActivity.java
@@ -3,7 +3,6 @@ package com.notificationbuzzer.fabuzaid21;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -15,59 +14,52 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.database.CharArrayBuffer;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
-import android.widget.ArrayAdapter;
-import android.widget.CursorAdapter;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class NotificationBuzzerActivity extends ListActivity implements OnItemClickListener {
 
 	private static final String NOTIFICATION_BUZZER_PACKAGE = NotificationBuzzerActivity.class.getPackage().getName();
+	private static final String ACTIVITY_NAME = NotificationBuzzerActivity.class.getSimpleName();
+
 	private BuzzDB base;
-	ArrayList<ResolveInfo> vibratedApps;
+	private List<ResolveInfo> vibratedApps;
 	private Dialog vibrationPatternDialog;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_notification_buzzer);
 
-		//Delete Database--for when I update the schema/triggers and need to test
-		//boolean test=this.getApplicationContext().deleteDatabase(LifeDB.DATABASE_NAME);
+		// Delete Database--for when I update the schema/triggers and need to
+		// test boolean test =
+		// this.getApplicationContext().deleteDatabase(LifeDB.DATABASE_NAME);
 
-		//open the database to find apps that have a vibration associated with them already.
+		// open the database to find apps that have a vibration associated with
+		// them already.
 		base = new BuzzDB(this);
 		base.open();
-
-
 
 		final ListView list = getListView();
 		final PackageManager pm = getPackageManager();
 
-
 		final Intent intent = new Intent(Intent.ACTION_MAIN, null);
 		intent.addCategory(Intent.CATEGORY_LAUNCHER);
 		final List<ResolveInfo> launcherApps = pm.queryIntentActivities(intent, PackageManager.PERMISSION_GRANTED);
-		for (ResolveInfo rInfo : launcherApps) {
-			Log.w("Installed Applications", rInfo.loadLabel(pm).toString());
-		}
 
-		vibratedApps=getVibratedApps(launcherApps, pm);
+		vibratedApps = getVibratedApps(launcherApps, pm);
 
-		SectionAdapter adapter=new SectionAdapter(this.getApplicationContext());
+		final SectionAdapter adapter = new SectionAdapter(this.getApplicationContext());
 
 		final AppAdapter unusedApps = new AppAdapter(this, filterSystemApps(launcherApps));
-		final AppAdapter usedApps= new AppAdapter(this, vibratedApps);
+		final AppAdapter usedApps = new AppAdapter(this, vibratedApps);
 
 		adapter.addSection("Add a pattern", unusedApps);
 		adapter.addSection("Review a pattern", usedApps);
@@ -81,7 +73,7 @@ public class NotificationBuzzerActivity extends ListActivity implements OnItemCl
 	@Override
 	protected void onResume() {
 		super.onResume();
-		//open accessibility menu
+		// open accessibility menu
 		checkAccessibility();
 	}
 
@@ -93,23 +85,25 @@ public class NotificationBuzzerActivity extends ListActivity implements OnItemCl
 		}
 	}
 
-	private ArrayList<ResolveInfo> getVibratedApps(List<ResolveInfo> launcherApps, PackageManager pm) {
+	private List<ResolveInfo> getVibratedApps(final List<ResolveInfo> launcherApps, final PackageManager pm) {
 
-		//We have the list of all apps, we want two lists. One with used apps, one with unused apps.
+		// We have the list of all apps, we want two lists. One with used apps,
+		// one with unused apps.
 
-		ArrayList<ResolveInfo> usedApps=new ArrayList<ResolveInfo>();
+		final List<ResolveInfo> usedApps = new ArrayList<ResolveInfo>();
 
-		ArrayList<String> appNames=new ArrayList<String>();
-		for (int x = 0; x < launcherApps.size(); x++)
-                  appNames.add(launcherApps.get(x).activityInfo.applicationInfo.packageName);
+		final List<String> appNames = new ArrayList<String>();
+		for (int x = 0; x < launcherApps.size(); x++) {
+			appNames.add(launcherApps.get(x).activityInfo.applicationInfo.packageName);
+		}
 
-		Cursor baseApps=base.queryAll(BuzzDB.DATABASE_APP_TABLE);
+		final Cursor baseApps = base.queryAll(BuzzDB.DATABASE_APP_TABLE);
 		baseApps.moveToFirst();
-		while(!baseApps.isAfterLast()) {
-			String name=baseApps.getString(BuzzDB.APP_INDEX_NAME);
+		while (!baseApps.isAfterLast()) {
+			final String name = baseApps.getString(BuzzDB.APP_INDEX_NAME);
 			if (appNames.contains(name)) {
-				int index=appNames.indexOf(name);
-				ResolveInfo item=launcherApps.get(index);
+				final int index = appNames.indexOf(name);
+				final ResolveInfo item = launcherApps.get(index);
 				usedApps.add(item);
 				appNames.set(index, "");
 				launcherApps.set(index, null);
@@ -132,10 +126,13 @@ public class NotificationBuzzerActivity extends ListActivity implements OnItemCl
 	private static List<ResolveInfo> filterSystemApps(final List<ResolveInfo> allApps) {
 		final List<ResolveInfo> notificationApps = new ArrayList<ResolveInfo>();
 		for (final ResolveInfo info : allApps) {
-			if ((info.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0 ||
-					info.activityInfo.applicationInfo.packageName.matches("com.android.(mms|contacts|calendar|email)")) {
+			if ((info.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0
+					|| info.activityInfo.applicationInfo.packageName
+							.matches("com.android.(mms|contacts|calendar|email)")) {
 
-				if (info.activityInfo.applicationInfo.packageName.equals(NOTIFICATION_BUZZER_PACKAGE)) continue;
+				if (info.activityInfo.applicationInfo.packageName.equals(NOTIFICATION_BUZZER_PACKAGE)) {
+					continue;
+				}
 				notificationApps.add(info);
 			}
 		}
@@ -149,58 +146,64 @@ public class NotificationBuzzerActivity extends ListActivity implements OnItemCl
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_notification_buzzer, menu);
 		return true;
 	}
 
-	public String getApplicationName(PackageInfo info) {
+	public String getApplicationName(final PackageInfo info) {
 		return info.applicationInfo.processName;
 	}
 
-	public void checkAccessibility() {
-		AccessibilityManager accMan =
-			(AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+	private void checkAccessibility() {
+		final AccessibilityManager accMan = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
 
-		List<AccessibilityServiceInfo> validList = accMan.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_HAPTIC);
+		final List<AccessibilityServiceInfo> validList = accMan
+				.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_HAPTIC);
 
-		for (int x = 0; x < validList.size(); x++) {
-			String[] packageNames = validList.get(x).packageNames;
-
-			for (int y = 0; y < packageNames.length; y++) {
-				if (packageNames[y].equals("com.notificationbuzzer.fabuzaid21"))
-					return;
+		for (final AccessibilityServiceInfo info : validList) {
+			// Log.w(ACTIVITY_NAME, info.getSettingsActivityName());
+			// }
+			// for (int x = 0; x < validList.size(); x++) {
+			// final String[] packageNames = validList.get(x).packageNames;
+			//
+			// for (int y = 0; y < packageNames.length; y++) {
+			// if (packageNames[y].equals(NOTIFICATION_BUZZER_PACKAGE)) {
+			if (info.getSettingsActivityName().endsWith(ACTIVITY_NAME)) {
+				return;
 			}
 		}
 
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		final AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
-                alert.setTitle("Accessability Settings");
-                alert.setMessage("You need to activate accessability settings to use Notification Buzzer. Continue?");
+		alert.setTitle("Accessability Settings");
+		alert.setMessage("You need to activate accessability settings to use Notification Buzzer. Continue?");
 
-                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                                enableAccessabilitySettings();
-                        }
-                });
+		alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(final DialogInterface dialog, final int whichButton) {
+				enableAccessabilitySettings();
+			}
+		});
 
-                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                                // Canceled.
-                        }
-                });
+		alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(final DialogInterface dialog, final int whichButton) {
+				// Canceled.
+			}
+		});
 
-                alert.show();
+		alert.show();
 	}
 
 	protected void enableAccessabilitySettings() {
-		Intent settingsIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+		final Intent settingsIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
 		startActivity(settingsIntent);
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+	public void onItemClick(final AdapterView<?> arg0, final View arg1, final int arg2, final long arg3) {
 		if (vibrationPatternDialog == null) {
 			vibrationPatternDialog = new Dialog(this, R.style.VibrationPatternDialogStyle);
 			vibrationPatternDialog.setContentView(R.layout.vibration_pattern);
