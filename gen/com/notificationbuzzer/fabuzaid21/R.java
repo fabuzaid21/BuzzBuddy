4160efec1d1f017f6c4ebee388ef782328ff290b
Adam Adler <damJadler@gmail.com>
2013-01-05 02:23:04 -0600

    Attempted to fix sectioned list and permission notification

--- gen/com/notificationbuzzer/fabuzaid21/R.java
+++ gen/com/notificationbuzzer/fabuzaid21/R.java
@@ -16,11 +16,13 @@ public final class R {
     public static final class id {
         public static final int app_icon=0x7f080000;
         public static final int app_name=0x7f080001;
-        public static final int menu_settings=0x7f080002;
+        public static final int list_header_title=0x7f080002;
+        public static final int menu_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_notification_buzzer=0x7f030000;
         public static final int app_row=0x7f030001;
+        public static final int list_header=0x7f030002;
     }
     public static final class menu {
         public static final int activity_notification_buzzer=0x7f070000;
--- src/com/notificationbuzzer/fabuzaid21/NotificationBuzzerActivity.java
+++ src/com/notificationbuzzer/fabuzaid21/NotificationBuzzerActivity.java
@@ -3,6 +3,8 @@ package com.notificationbuzzer.fabuzaid21;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
@@ -12,17 +14,23 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.CharArrayBuffer;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.view.accessibility.AccessibilityManager;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class NotificationBuzzerActivity extends ListActivity {
 
 	private BuzzDB base;
+	ArrayList<ResolveInfo> vibratedApps;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,23 +44,73 @@ public class NotificationBuzzerActivity extends ListActivity {
 		base = new BuzzDB(this);
 		base.open();
 
-		Cursor currentVibes=base.queryAll(BuzzDB.DATABASE_APP_TABLE);
-		currentVibes.moveToFirst();
-
+		
+				
 		final ListView list = getListView();
 		final PackageManager pm = getPackageManager();
+						
+		
 		final Intent intent = new Intent(Intent.ACTION_MAIN, null);
 		intent.addCategory(Intent.CATEGORY_LAUNCHER);
 		final List<ResolveInfo> launcherApps = pm.queryIntentActivities(intent, PackageManager.PERMISSION_GRANTED);
 		for (ResolveInfo rInfo : launcherApps) {
 			Log.w("Installed Applications", rInfo.loadLabel(pm).toString());
 		}
-
-		final AppAdapter adapter = new AppAdapter(this, filterSystemApps(launcherApps));
+		
+		vibratedApps=getVibratedApps(launcherApps, pm);
+		
+		SectionAdapter adapter=new SectionAdapter(this.getApplicationContext());
+		
+		final AppAdapter unusedApps = new AppAdapter(this, filterSystemApps(launcherApps));
+		final AppAdapter usedApps= new AppAdapter(this, vibratedApps);
+		
+		adapter.addSection("Add a pattern", unusedApps);
+		adapter.addSection("Review a pattern", usedApps);
+		
 		list.setAdapter(adapter);
 
 		//open accessibility menu
 		checkAccessibility();
+	}
+
+	private ArrayList<ResolveInfo> getVibratedApps(List<ResolveInfo> launcherApps, PackageManager pm) {
+		
+		//We have the list of all apps, we want two lists. One with used apps, one with unused apps.
+		
+		ArrayList<ResolveInfo> usedApps=new ArrayList<ResolveInfo>();
+		
+		ArrayList<String> appNames=new ArrayList<String>();		
+		for(int x=0;x<launcherApps.size();x++)
+		appNames.add(launcherApps.get(x).activityInfo.applicationInfo.packageName);
+				
+		Cursor baseApps=base.queryAll(BuzzDB.DATABASE_APP_TABLE);
+		baseApps.moveToFirst();
+		while(!baseApps.isAfterLast())
+		{
+			String name=baseApps.getString(BuzzDB.APP_INDEX_NAME);
+			if(appNames.contains(name))
+			{
+				int index=appNames.indexOf(name);
+				ResolveInfo item=launcherApps.get(index);
+				usedApps.add(item);
+				appNames.set(index, "");
+				launcherApps.set(index, null);
+			}
+			
+			baseApps.moveToNext();
+		}
+		
+		for(int x=0;x<launcherApps.size();x++)
+		{
+			if(launcherApps.get(x)==null)
+			{
+				launcherApps.remove(x);
+				x--;
+			}
+		}
+				
+		return usedApps;
+		
 	}
 
 	private static List<ResolveInfo> filterSystemApps(final List<ResolveInfo> allApps) {
@@ -66,6 +124,14 @@ public class NotificationBuzzerActivity extends ListActivity {
 		return notificationApps;
 	}
 
+	public void onDestroy()
+	{
+		base.close();
+		super.onDestroy();
+	}
+
+	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -75,16 +141,27 @@ public class NotificationBuzzerActivity extends ListActivity {
 
 	public String getApplicationName(PackageInfo info)
 	{
-		return info.applicationInfo.processName;
+		return info.applicationInfo.packageName;
 	}
 
 	public void checkAccessibility()
 	{
 		AccessibilityManager accMan =
 			(AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
-		final boolean accessEnabled = accMan.isEnabled();
-
-		if (!accessEnabled) {
+		
+		List<AccessibilityServiceInfo> validList=accMan.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_HAPTIC);
+		
+		for(int x=0;x<validList.size();x++)
+		{
+			String[]packageNames=validList.get(x).packageNames;
+			
+			for(int y=0;y<packageNames.length;y++)
+			{
+				if(packageNames[y].equals("com.notificationbuzzer.fabuzaid21"))
+					return;
+			}
+		}
+		
 			AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
 			alert.setTitle("Accessability Settings");
@@ -104,9 +181,7 @@ public class NotificationBuzzerActivity extends ListActivity {
 
 			alert.show();
 
-		}
-
-
+		
 	}
 
 	protected void enableAccessabilitySettings() {
