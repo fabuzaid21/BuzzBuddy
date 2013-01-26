9e2ef92f100b8096cc430c676cabbedd13212d77
Firas Abuzaid <fabuzaid21@gmail.com>
2013-01-26 11:33:19 -0800

    CheckBox is now easily clickable on HoneyComb and above, too

---
+++ src/com/notificationbuzzer/fabuzaid21/CheckableRowItem.java
@@ -0,0 +1,54 @@
+package com.notificationbuzzer.fabuzaid21;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class CheckableRowItem extends LinearLayout {
+
+	private static final String TAG = CheckableRowItem.class.getSimpleName();
+
+	private EasyTargetCheckBox checkBox;
+	private int checkBoxLeft;
+	private int checkBoxWidth;
+
+	public CheckableRowItem(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+
+	}
+
+	@Override
+	public boolean onTouchEvent(final MotionEvent e) {
+		if (checkBox == null) {
+			initializeCheckBox();
+		}
+
+		if (checkBox.getVisibility() == View.GONE) {
+			Log.d(TAG, "checkBox not showing");
+			return false;
+		}
+		if (e.getX() > (checkBoxLeft + checkBoxWidth)) {
+			Log.d(TAG, "outside of range");
+			return false;
+		}
+		if (e.getAction() == MotionEvent.ACTION_DOWN) {
+			checkBox.setPressed(true);
+			return true;
+		} else if (e.getAction() == MotionEvent.ACTION_UP) {
+			Log.d(TAG, "checkbox is showing");
+			checkBox.setPressed(false);
+			checkBox.performClick();
+			return true;
+		}
+		return false;
+	}
+
+	private void initializeCheckBox() {
+		checkBox = (EasyTargetCheckBox) getChildAt(0);
+		checkBoxLeft = checkBox.getLeft();
+		checkBoxWidth = checkBox.getWidth();
+	}
+}
